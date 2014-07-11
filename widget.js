bd0dc45ee40a2c0e3a9d80af29c5f3e5ac73ba71
SirArslaan <arhamoja@gmail.com>
2014-07-11 19:41:15 +0200

    Final try... I failed

--- widget.js
+++ widget.js
@@ -28,7 +28,23 @@ var load = function (link, prefix){
 
     xhr.send();
 };
+var loadSearch=function(link,prefix){
+  var xhr =new XMLHttpRequest();
+  xhr.open('GET', "https://api.spotify.com/v1/"+prefix+link);
+  xhr.setRequestHeader('Accept', 'application/json'); //why accept
+  xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          response = JSON.parse(this.response);
+          console.log('onreadystatechange response', response);
+          putResults(response.tracks.items);
+        }
+      }
+    };
 
+
+  xhr.send();
+}
 
 load(link, "tracks/");
 
@@ -41,11 +57,20 @@ audio.setAttribute('src', link);
 
 
 var parseSearch=function(query){
-
+  var newValue="";
+  for(var i=0;i<query.length;i++){
+    if(query[i]==" "){
+      newValue+="%20";
+    }
+    else{
+      newValue+=query[i];
+    }
+  }
+  return newValue;
 };
 
 document.addEventListener('submit', function (evt){
-    console.log(evt);
+  console.log(evt);
     if(evt.target.id==='track'){
       evt.preventDefault();
       link= document.querySelector('input').value;
@@ -53,24 +78,31 @@ document.addEventListener('submit', function (evt){
 
     }
 
-    else if(evt.target.id==='searchItem'){
+    else if(evt.target.id==='search'){
       evt.preventDefault();
-      var words= document.querySelector('#searchItem').value;
-      
-      var wordsArray=words.split(" ");
-      console.log(wordsArray);
-
-      // load(link, ""):
-
-
-
+      var words= evt.target.searchItem.value;
+      words+="&type=track";
+      link=parseSearch(words);
+      loadSearch(link, "search/?q=");
     }
 
 
   
 
 });
-
+var putResults=function(array){
+  var ul=document.querySelector("ul");
+  for(var i=array.length-1;i>=0;i--){
+    var artist=document.createElement("span");
+    var song=document.createElement("span");
+    artist.className="artist";
+    song.className="song";
+    artist.textContent=array[i].artist[0].name;
+    artist.textContent=array[i].name;
+    ul.appendChild(artist);
+    ul.appendChild(song);
+  }
+}
 
 
 
